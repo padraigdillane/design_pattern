2fa85d8a8e75b47b99c69ff90612dbc4b5af5712
padraigdillane <padraigdillane@gmail.com>
2015-12-17 15:36:52 +0000

    test complete for goodBoxer

--- src/template/tests/BoxerTrainingTest.java
+++ src/template/tests/BoxerTrainingTest.java
@@ -1,50 +1,40 @@
 package template.tests;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import template.GoodBoxer;
 
 import static org.junit.Assert.*;
 
 public class BoxerTrainingTest {
+    GoodBoxer goodBoxer;
 
     @Before
     public void setUp() throws Exception {
-
+        goodBoxer = new GoodBoxer();
     }
 
     @After
     public void tearDown() throws Exception {
-
+        goodBoxer = null;
     }
 
-    @Test
-    public void testStartTrainingSession() throws Exception {
-
-    }
 
     @Test
-    public void testDoCardio() throws Exception {
-
+    public void boxerIsDoingCardio() throws Exception {
+        Assert.assertEquals(goodBoxer.doCardio(), true);
     }
 
     @Test
     public void testDoWeights() throws Exception {
-
+        Assert.assertEquals(goodBoxer.doWeights(),true);
     }
 
     @Test
     public void testDoBoxingSession() throws Exception {
-
+        Assert.assertEquals(goodBoxer.doBoxingSession(), true);
     }
 
-    @Test
-    public void testStartTraing() throws Exception {
-
-    }
-
-    @Test
-    public void testEndTraining() throws Exception {
-
-    }
 }
